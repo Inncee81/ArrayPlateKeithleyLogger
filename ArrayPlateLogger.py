cf2111b30b5a60130055f34a29eb8533a30425be
MatthewAHarvey <matthew.a.harvey@gmail.com>
2020-11-24 14:38:56 +0000

    Saving Data
    Updated DMM code as well

--- ArrayPlateLogger.py
+++ ArrayPlateLogger.py
@@ -41,7 +41,9 @@ def connectKeithley(inst_ip = "192.168.0.2",inst_port = "1394",timeout=10E3):
     Sets up VISA comms with a TCP/IP VISA resurce at the given ip adress and port,
     then queries the identitiy of the resource and prints any response
     
-    Keyword arguements
+    Also starts measuring the voltage on the middle pin of the array plate (125 on the multiplexer)
+    
+    Keyword arguments
     timeout -- the timeout of the resource (default 10E3 ms)
     """
     rm = pyvisa.ResourceManager('@py')
@@ -49,6 +51,19 @@ def connectKeithley(inst_ip = "192.168.0.2",inst_port = "1394",timeout=10E3):
     dmm.read_termination = '\n' #must set the termination character for the instrument 
     dmm.timeout=timeout
     print(dmm.query('*IDN?'))
+    setup = [
+        '*RST', # resets the device
+        'TRAC:CLE', # clears the buffer
+        "ROUT:OPEN:ALL", # makes sure all connections on the multiplexer are open (disconnected)
+        "ROUT:CLOS (@125)", # connects the middle pin of the array plate to the meter
+        "FUNC 'VOLT',(@125)", # makes sure it's a voltage reading we are taking.
+        "INIT:CONT ON", # continuously sample so get updates on front panel.
+        'FORM:ELEM READ',#, UNIT, TST, CHAN, LIM' #lots of spare data, for some reason?
+    ]
+    for cmd in setup:
+        dmm.write(cmd)
+#         time.sleep(.1)
+    
     return dmm
 
 def disconnectKeithley(dmm):
@@ -60,19 +75,69 @@ def disconnectVisa():
     pyvisa.ResourceManager('@py').close()
     return
 
+def readMiddlePin(dmm, read = False):
 
-def read_voltages(dmm,delay=0):
+    if read:
+        comArray = [
+            "TRAC:CLE", # Clear buffer.
+            "INIT:CONT OFF", # Disable continuous initiation.
+            "TRIG:SOUR IMM", # Select the immediate control source.
+            "TRIG:COUN 1", # Set to perform one scan.
+            "SAMP:COUN 1", # Set to scan 10 channels.
+            "ROUT:SCAN (@125)", # Set scan to channel 125
+            "ROUT:SCAN:TSO IMM", # Start scan when enabled and triggered.
+            "ROUT:SCAN:LSEL INT", # Enable scan.
+            'FORM:ELEM READ',#, UNIT, TST, CHAN, LIM' #lots of spare data, for some reason?
+        ]
+        for cmd in comArray:
+            dmm.write(cmd)
+            time.sleep(0.1)
+        voltage=dmm.query_ascii_values('READ?')[0]
+    
+    setup = [
+        '*RST', # resets the device
+        'TRAC:CLE', # clears the buffer
+        "ROUT:OPEN:ALL", # makes sure all connections on the multiplexer are open (disconnected)
+        "ROUT:CLOS (@125)", # connects the middle pin of the array plate to the meter
+        "FUNC 'VOLT',(@125)", # makes sure it's a voltage reading we are taking.
+        "INIT:CONT ON", # continuously sample so get updates on front panel.
+        'FORM:ELEM READ',#, UNIT, TST, CHAN, LIM' #lots of spare data, for some reason?
+    ]
+    for cmd in setup:
+        dmm.write(cmd)
+        time.sleep(0.1)
+
+    if read:
+        return voltage
+    else:
+        return None
+
+def read_voltages(dmm,centre5x5 = False, delay=0):
     """
     Scans 49 channels of a keithley 2701 DMM and returns the resposnes as an array
     (7x7)
     """
     setup = [
-            '*RST',
+        '*RST',
+        'TRAC:CLE',
+        "ROUT:OPEN:ALL", # makes sure all connections on the multiplexer are open (disconnected)
+        'FORM:ELEM READ'#, UNIT, TST, CHAN, LIM' #lots of spare data, for some reason?
+    ]   
+    voltage_setup = []
+    if centre5x5:
+        voltage_setup = [
             'TRAC:CLE',
-            'FORM:ELEM READ'#, UNIT, TST, CHAN, LIM' #lots of spare data, for some reason?
-            ]
-
-    voltage_setup = [
+            'INIT:CONT OFF',
+            'SENS:FUNC "VOLT", (@109:113,116:120,123:127,130:134,137:140,201)',
+            'ROUT:SCAN (@109:113,116:120,123:127,130:134,137:140,201)',
+            'TRIG:COUN 1',
+            'TRIG:DEL '+str(delay),
+            'SAMP:COUN 25',
+            'TRIG:SOUR IMM',
+            'ROUT:SCAN:LSEL INT',
+        ]
+    else:
+        voltage_setup = [
             'TRAC:CLE',
             'INIT:CONT OFF',
             'SENS:FUNC "VOLT", (@101:140,201:209)',
@@ -81,17 +146,24 @@ def read_voltages(dmm,delay=0):
             'TRIG:DEL '+str(delay),
             'SAMP:COUN 49',
             'TRIG:SOUR IMM',
-            'ROUT:SCAN:LSEL INT',]
+            'ROUT:SCAN:LSEL INT',
+        ]
 
     for cmd in [*setup,*voltage_setup]:
         dmm.write(cmd)
-        time.sleep(.1)
-    #         print(cmd)
+        time.sleep(0.1)
+    
 
     voltages=dmm.query_ascii_values('READ?')
     # voltages = [voltages[7*i:7*i+7] for i in range(7)] # reshape to 2d list
+    # 
     voltages=np.array(voltages)
-    voltages=voltages.reshape([7,7])
+    if centre5x5:
+        voltages=voltages.reshape([5,5])
+    else:
+        voltages=voltages.reshape([7,7])
+
+    readMiddlePin(dmm)
     if voltages.any()>1E10:
         return 'error'
 
@@ -111,14 +183,22 @@ def plotBase(readings, height = 600, width = 600, title = ''):
 
     fig = go.Figure(data =
         go.Contour(
+            colorscale='RdBu',
             z=readings,
+            # zauto = False,
+            zmin=-np.max(np.abs(readings)),
+            zmax=np.max(np.abs(readings)),
             zmid=0,
-            ncontours=20
+            ncontours=21,
         ),
         layout = layout
     )
-    x = np.array([n for n in range(7) for n in range(7)])
-    y = np.array([i for i in range(7) for n in range(7)])
+    if len(readings) == 5:
+        x = np.array([n for n in range(5) for n in range(5)])
+        y = np.array([i for i in range(5) for n in range(5)])
+    else:
+        x = np.array([n for n in range(7) for n in range(7)])
+        y = np.array([i for i in range(7) for n in range(7)])
     fig.add_scatter(x=x,y=y, mode='markers',
                     marker = dict(color = 'rgb(0, 0, 0)', size = 5,)
                    )
@@ -184,13 +264,13 @@ class arrayPlateMeasurement:
         self.data.append(entry)
         print(f'Number of entries: {len(self.data)}')
     
-    def readVoltages(self):
-        voltages = read_voltages(self.dmm)
+    def readVoltages(self, centre5x5 = False):
+        voltages = read_voltages(self.dmm, centre5x5)
         return voltages
 
-    def readCurrents(self):
+    def readCurrents(self, centre5x5 = False):
         area = (11.72/10/2)**2*np.pi
-        voltages = read_voltages(self.dmm)
+        voltages = read_voltages(self.dmm, centre5x5)
         currents = voltages * 10 /area
         # currents = [voltage*10/area for voltage in read_voltages(self.dmm)]
         return currents
